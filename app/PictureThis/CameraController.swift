975f1aedf3c01a18145de560be89e002a83888c8
Itai Reuveni <lilnamcor@berkeley.edu>
2016-12-22 13:49:50 -0800

    does cool shit now

--- app/PictureThis/CameraController.swift
+++ app/PictureThis/CameraController.swift
@@ -9,8 +9,9 @@
 import UIKit
 import AVFoundation
 
-class CameraController: UIViewController {
+class CameraController: UIViewController, UITextFieldDelegate {
 
+    // buttons
     @IBOutlet weak var notifications: UIButton!
     @IBOutlet weak var capture: UIButton!
     @IBOutlet weak var slider: UISlider!
@@ -21,73 +22,128 @@ class CameraController: UIViewController {
     @IBOutlet weak var flash: UIButton!
     @IBOutlet weak var answer: UITextField!
     
-        
+    // flags to remember which buttons have been pressed
+    var captureMode = true
+    var blurPressed = false
+    var zoomPressed = false
+    var brightnessPressed = false
+    var answerPressed = false
+    var sendAction = false
+    
+    // stuff to capture images/video and show on the screen
     @IBOutlet weak var imageView: UIImageView!
     var captureSession : AVCaptureSession?
     var stillImageOutput : AVCaptureStillImageOutput?
     var previewLayer : AVCaptureVideoPreviewLayer?
+    
+    @IBAction func sendAction(_ sender: Any) {
+        if (captureMode) {
+            
+        } else {
+            if (!answerPressed || answer.text == "") {
+                sendAction = true
+                answer.becomeFirstResponder()
+            } else {
+                self.performSegue(withIdentifier: "FriendsList", sender:self)
+            }
+        }
+    }
+    
+    @IBAction func answerAction(_ sender: Any) {
+        if (!answerPressed) {
+            answer.text = ""
+            answerPressed = true
+        }
+    }
+    
+    func resetAnswer() {
+        answer.resignFirstResponder()
+        if (answer.text == "") {
+            answer.text = "_ _ _ _"
+            answerPressed = false
+        }
+    }
+    
+    func resetButtons(button: UIButton) {
+        
+    }
     
     @IBAction func sliderAction(_ sender: Any) {
         print(slider.value)
     }
     
     @IBAction func blurAction(_ sender: Any) {
-        slider.isHidden = false
-        slider.isEnabled = true
+        resetAnswer()
+        if (!blurPressed){
+            slider.isHidden = false
+            slider.isEnabled = true
+            brightnessPressed = false
+            zoomPressed = false
+            blurPressed = true
+        } else {
+            slider.isHidden = true
+            slider.isEnabled = false
+            blurPressed = false
+        }
     }
     
-    func disableButton(element: UIButton) {
-        element.isHidden = true
-        element.isEnabled = false
+    @IBAction func zoomAction(_ sender: Any) {
+        resetAnswer()
+        if (!zoomPressed){
+            zoom.setBackgroundImage(#imageLiteral(resourceName: "magnifyingClicked"), for: .normal)
+            slider.isHidden = false
+            slider.isEnabled = true
+            brightnessPressed = false
+            zoomPressed = true
+            blurPressed = false
+        } else {
+            zoom.setBackgroundImage(#imageLiteral(resourceName: "magnifying"), for: .normal)
+            slider.isHidden = true
+            slider.isEnabled = false
+            zoomPressed = false
+        }
     }
     
-    func enableButton(element: UIButton) {
-        element.isHidden = false
-        element.isEnabled = true
+    @IBAction func brightnessAction(_ sender: Any) {
+        resetAnswer()
+        if (!brightnessPressed){
+            slider.isHidden = false
+            slider.isEnabled = true
+            brightnessPressed = true
+            zoomPressed = false
+            blurPressed = false
+        } else {
+            slider.isHidden = true
+            slider.isEnabled = false
+            brightnessPressed = false
+        }
     }
     
+    // if in capture mode its flash if not then its an X to go back to capture mode
     @IBAction func flashAction(_ sender: Any) {
-        self.captureSession?.startRunning()
-        notifications.setTitle("notifications", for: .normal)
-        settings.setTitle("settings", for: .normal)
-        flash.setTitle("flash", for: .normal)
-        disableButton(element: zoom)
-        disableButton(element: brightness)
-        disableButton(element: blur)
-        answer.isHidden = true
-        self.previewLayer?.isHidden = false
-        enableButton(element: capture)
+        resetAnswer()
+        if (captureMode) {
+        } else {
+            self.captureSession?.startRunning()
+            notifications.setTitle("notifications", for: .normal)
+            settings.setTitle("settings", for: .normal)
+            flash.setTitle("flash", for: .normal)
+            disableButton(element: zoom)
+            disableButton(element: brightness)
+            disableButton(element: blur)
+            answer.isHidden = true
+            slider.isHidden = true
+            slider.isEnabled = false
+            self.previewLayer?.isHidden = false
+            enableButton(element: capture)
+            captureMode = true
+            zoom.setBackgroundImage(#imageLiteral(resourceName: "magnifying"), for: .normal)
+        }
     }
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        slider.isContinuous = true
-        
-        notifications.setTitle("notifications", for: .normal)
-        settings.setTitle("settings", for: .normal)
-        flash.setTitle("flash", for: .normal)
-        
-        
-        disableButton(element: zoom)
-        disableButton(element: brightness)
-        disableButton(element: blur)
-        answer.isHidden = true
-        slider.isHidden = true
-        slider.isEnabled = false
-        // Do any additional setup after loading the view.
-    }
-    
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
-    override func viewDidAppear(_ animated: Bool) {
-        super.viewDidAppear(animated)
-        previewLayer?.frame = self.view.layer.bounds
-    }
-    
+    // takes and saves the picture
     @IBAction func captureAction(_ sender: Any) {
+        captureMode = false
         if let videoConnection = stillImageOutput?.connection(withMediaType: AVMediaTypeVideo) {
             stillImageOutput?.captureStillImageAsynchronously(from: videoConnection) {
                 (imageDataSampleBuffer, error) -> Void in
@@ -106,6 +162,43 @@ class CameraController: UIViewController {
         disableButton(element: capture)
         
         answer.isHidden = false
+    }
+    
+    // this function is called when you press enter on a text field
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        if (sendAction) {
+            self.performSegue(withIdentifier: "FriendsList", sender:self)
+        }
+        return true
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        slider.isContinuous = true
+        
+        notifications.setTitle("notifications", for: .normal)
+        settings.setTitle("settings", for: .normal)
+        flash.setTitle("flash", for: .normal)
+        
+        
+        disableButton(element: zoom)
+        disableButton(element: brightness)
+        disableButton(element: blur)
+        answer.isHidden = true
+        slider.isHidden = true
+        slider.isEnabled = false
+        self.answer.delegate = self;
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        previewLayer?.frame = self.view.layer.bounds
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -148,6 +241,16 @@ class CameraController: UIViewController {
         }
         
         
+    }
+    
+    func disableButton(element: UIButton) {
+        element.isHidden = true
+        element.isEnabled = false
+    }
+    
+    func enableButton(element: UIButton) {
+        element.isHidden = false
+        element.isEnabled = true
     }
 
 }
