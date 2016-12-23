836445077b2f043ad43a725e95e824db9e608760
Itai Reuveni <lilnamcor@berkeley.edu>
2016-12-22 19:38:22 -0800

    added lots of cool animations

--- app/PictureThis/CameraController.swift
+++ app/PictureThis/CameraController.swift
@@ -25,8 +25,11 @@ class CameraController: UIViewController, UITextFieldDelegate {
     // flags to remember which buttons have been pressed
     var captureMode = true
     var blurPressed = false
+    var blurValue = 0.5
     var zoomPressed = false
+    var zoomValue = 0.5
     var brightnessPressed = false
+    var brightnessValue = 0.5
     var answerPressed = false
     var sendAction = false
     
@@ -36,9 +39,40 @@ class CameraController: UIViewController, UITextFieldDelegate {
     var stillImageOutput : AVCaptureStillImageOutput?
     var previewLayer : AVCaptureVideoPreviewLayer?
     
+    @IBAction func notificationAction(_ sender: Any) {
+        animationContract(button: notifications)
+        if (captureMode) {
+            self.performSegue(withIdentifier: "Notifications", sender:self)
+        } else {
+            slider.isEnabled = false
+            slider.isHidden = true
+            if (blurPressed) {
+                blurValue = Double(0)
+                zoomPressed = false
+                brightnessPressed = false
+                zoom.setImage(#imageLiteral(resourceName: "Logo"), for: .normal)
+                brightness.setImage(#imageLiteral(resourceName: "Brightness"), for: .normal)
+            } else if (zoomPressed) {
+                zoomValue = Double(0)
+                blurPressed = false
+                brightnessPressed = false
+                blur.setImage(#imageLiteral(resourceName: "Blur"), for: .normal)
+                brightness.setImage(#imageLiteral(resourceName: "Brightness"), for: .normal)
+            } else {
+                brightnessValue = Double(0)
+                blurPressed = false
+                zoomPressed = false
+                blur.setImage(#imageLiteral(resourceName: "Blur"), for: .normal)
+                zoom.setImage(#imageLiteral(resourceName: "Logo"), for: .normal)
+            }
+            blurPressed = false
+            zoomPressed = false
+            brightnessPressed = false
+        }
+    }
     @IBAction func sendAction(_ sender: Any) {
         if (captureMode) {
-            
+            self.performSegue(withIdentifier: "Settings", sender:self)
         } else {
             if (!answerPressed || answer.text == "") {
                 sendAction = true
@@ -64,58 +98,97 @@ class CameraController: UIViewController, UITextFieldDelegate {
         }
     }
     
-    func resetButtons(button: UIButton) {
-        
-    }
-    
     @IBAction func sliderAction(_ sender: Any) {
-        print(slider.value)
+        if (blurPressed) {
+            blurValue = Double(slider.value)
+        } else if (zoomPressed) {
+            zoomValue = Double(slider.value)
+        } else {
+            brightnessValue = Double(slider.value)
+        }
     }
     
     @IBAction func blurAction(_ sender: Any) {
+        animationContract(button: blur)
         resetAnswer()
         if (!blurPressed){
             slider.isHidden = false
             slider.isEnabled = true
+            slider.value = Float(blurValue)
             brightnessPressed = false
             zoomPressed = false
             blurPressed = true
+            blur.setImage(#imageLiteral(resourceName: "Blur"), for: .normal)
+            zoom.setImage(#imageLiteral(resourceName: "Logo Dark"), for: .normal)
+            brightness.setImage(#imageLiteral(resourceName: "Brightness Dark"), for: .normal)
+            notifications.isEnabled = true
+            notifications.isHidden = false
         } else {
             slider.isHidden = true
             slider.isEnabled = false
             blurPressed = false
+            blur.setImage(#imageLiteral(resourceName: "Blur"), for: .normal)
+            zoom.setImage(#imageLiteral(resourceName: "Logo"), for: .normal)
+            brightness.setImage(#imageLiteral(resourceName: "Brightness"), for: .normal)
+            notifications.isEnabled = false
+            notifications.isHidden = true
         }
     }
     
     @IBAction func zoomAction(_ sender: Any) {
+        animationContract(button: zoom)
         resetAnswer()
         if (!zoomPressed){
-            zoom.setBackgroundImage(#imageLiteral(resourceName: "magnifyingClicked"), for: .normal)
             slider.isHidden = false
             slider.isEnabled = true
+            slider.value = Float(zoomValue)
             brightnessPressed = false
             zoomPressed = true
             blurPressed = false
+            blur.setImage(#imageLiteral(resourceName: "Blur Dark"), for: .normal)
+            zoom.setImage(#imageLiteral(resourceName: "Logo"), for: .normal)
+            brightness.setImage(#imageLiteral(resourceName: "Brightness Dark"), for: .normal)
+            notifications.setImage(#imageLiteral(resourceName: "Undo"), for: .normal)
+            notifications.isEnabled = true
+            notifications.isHidden = false
         } else {
-            zoom.setBackgroundImage(#imageLiteral(resourceName: "magnifying"), for: .normal)
             slider.isHidden = true
             slider.isEnabled = false
             zoomPressed = false
+            blur.setImage(#imageLiteral(resourceName: "Blur"), for: .normal)
+            zoom.setImage(#imageLiteral(resourceName: "Logo"), for: .normal)
+            brightness.setImage(#imageLiteral(resourceName: "Brightness"), for: .normal)
+            notifications.setImage(#imageLiteral(resourceName: "Undo"), for: .normal)
+            notifications.isEnabled = false
+            notifications.isHidden = true
         }
     }
     
     @IBAction func brightnessAction(_ sender: Any) {
+        animationContract(button: brightness)
         resetAnswer()
         if (!brightnessPressed){
             slider.isHidden = false
             slider.isEnabled = true
+            slider.value = Float(brightnessValue)
             brightnessPressed = true
             zoomPressed = false
             blurPressed = false
+            blur.setImage(#imageLiteral(resourceName: "Blur Dark"), for: .normal)
+            zoom.setImage(#imageLiteral(resourceName: "Logo Dark"), for: .normal)
+            brightness.setImage(#imageLiteral(resourceName: "Brightness"), for: .normal)
+            notifications.setImage(#imageLiteral(resourceName: "Undo"), for: .normal)
+            notifications.isEnabled = true
+            notifications.isHidden = false
         } else {
             slider.isHidden = true
             slider.isEnabled = false
             brightnessPressed = false
+            blur.setImage(#imageLiteral(resourceName: "Blur"), for: .normal)
+            zoom.setImage(#imageLiteral(resourceName: "Logo"), for: .normal)
+            brightness.setImage(#imageLiteral(resourceName: "Brightness"), for: .normal)
+            notifications.isEnabled = false
+            notifications.isHidden = true
         }
     }
     
@@ -123,27 +196,43 @@ class CameraController: UIViewController, UITextFieldDelegate {
     @IBAction func flashAction(_ sender: Any) {
         resetAnswer()
         if (captureMode) {
+            animationContract(button: flash)
         } else {
+            UIView.animate(withDuration: 0,
+                animations: {
+                    self.flash.transform = CGAffineTransform(scaleX: 1, y: 1)
+                }
+            )
+            settings.setImage(#imageLiteral(resourceName: "Settings"), for: .normal)
+            flash.setImage(#imageLiteral(resourceName: "Flash"), for: .normal)
+            notifications.isEnabled = true
+            notifications.isHidden = false
+            notifications.setImage(#imageLiteral(resourceName: "Notification"), for: .normal)
             self.captureSession?.startRunning()
-            notifications.setTitle("notifications", for: .normal)
-            settings.setTitle("settings", for: .normal)
-            flash.setTitle("flash", for: .normal)
+            blur.setImage(#imageLiteral(resourceName: "Blur"), for: .normal)
+            zoom.setImage(#imageLiteral(resourceName: "Logo"), for: .normal)
+            brightness.setImage(#imageLiteral(resourceName: "Brightness"), for: .normal)
+            disableButton(element: blur)
             disableButton(element: zoom)
             disableButton(element: brightness)
-            disableButton(element: blur)
+            blurPressed = false
+            zoomPressed = false
+            brightnessPressed = false
             answer.isHidden = true
             slider.isHidden = true
             slider.isEnabled = false
             self.previewLayer?.isHidden = false
             enableButton(element: capture)
             captureMode = true
-            zoom.setBackgroundImage(#imageLiteral(resourceName: "magnifying"), for: .normal)
         }
     }
     
     // takes and saves the picture
     @IBAction func captureAction(_ sender: Any) {
         captureMode = false
+        settings.setImage(#imageLiteral(resourceName: "Send"), for: .normal)
+        notifications.isEnabled = false
+        notifications.isHidden = true
         if let videoConnection = stillImageOutput?.connection(withMediaType: AVMediaTypeVideo) {
             stillImageOutput?.captureStillImageAsynchronously(from: videoConnection) {
                 (imageDataSampleBuffer, error) -> Void in
@@ -153,9 +242,7 @@ class CameraController: UIViewController, UITextFieldDelegate {
                 self.imageView.image = UIImage(data: imageData!)
             }
         }
-        notifications.setTitle("cancel", for: .normal)
-        settings.setTitle("send", for: .normal)
-        flash.setTitle("X", for: .normal)
+        flash.setImage(#imageLiteral(resourceName: "X"), for: .normal)
         enableButton(element: zoom)
         enableButton(element: brightness)
         enableButton(element: blur)
@@ -176,11 +263,7 @@ class CameraController: UIViewController, UITextFieldDelegate {
     override func viewDidLoad() {
         super.viewDidLoad()
         slider.isContinuous = true
-        
-        notifications.setTitle("notifications", for: .normal)
-        settings.setTitle("settings", for: .normal)
-        flash.setTitle("flash", for: .normal)
-        
+        slider.maximumTrackTintColor = UIColor.darkGray
         
         disableButton(element: zoom)
         disableButton(element: brightness)
@@ -251,6 +334,95 @@ class CameraController: UIViewController, UITextFieldDelegate {
     func enableButton(element: UIButton) {
         element.isHidden = false
         element.isEnabled = true
+    }
+    
+    // All the code for expanding and contracting the buttons
+    func animationExpand(button: UIButton) {
+        UIView.animate(withDuration: 0.2,
+            animations: {
+                button.transform = CGAffineTransform(scaleX: 1.3, y: 1.3)
+            }
+        )
+    }
+    
+    func animationContract(button: UIButton) {
+        UIView.animate(withDuration: 0.2,
+            animations: {
+                button.transform = CGAffineTransform(scaleX: 1, y: 1)
+            }
+        )
+    }
+    
+    @IBAction func blurExpandAction(_ sender: Any) {
+        animationExpand(button: blur)
+    }
+    
+    @IBAction func blurDragExitAction(_ sender: Any) {
+        animationContract(button: blur)
+    }
+    
+    @IBAction func blurDragEnterAction(_ sender: Any) {
+        animationExpand(button: blur)
+    }
+    
+    @IBAction func zoomExpandAction(_ sender: Any) {
+        animationExpand(button: zoom)
+    }
+    
+    @IBAction func zoomDragExitAction(_ sender: Any) {
+        animationContract(button: zoom)
+    }
+    
+    @IBAction func zoomDragEnterAction(_ sender: Any) {
+        animationExpand(button: zoom)
+    }
+    
+    @IBAction func brightnessExpandAction(_ sender: Any) {
+        animationExpand(button: brightness)
+    }
+    
+    @IBAction func brightnessDragExitAction(_ sender: Any) {
+        animationContract(button: brightness)
+    }
+    
+    @IBAction func brightnessDragEnterAction(_ sender: Any) {
+        animationExpand(button: brightness)
+    }
+    
+    @IBAction func flashExpandAction(_ sender: Any) {
+        animationExpand(button: flash)
+    }
+    
+    @IBAction func flashDragExitAction(_ sender: Any) {
+        animationContract(button: flash)
+    }
+    
+    @IBAction func flashDragEnterAction(_ sender: Any) {
+        animationExpand(button: flash)
+    }
+    
+    @IBAction func sendExpandAction(_ sender: Any) {
+        animationExpand(button: settings)
+    }
+    
+    @IBAction func sendDragExitAction(_ sender: Any) {
+        animationContract(button: settings)
+    }
+    
+    @IBAction func sendDragEnterAction(_ sender: Any) {
+        animationExpand(button: settings)
+    }
+    
+    @IBAction func notificationExpandAction(_ sender: Any) {
+        animationExpand(button: notifications)
+    }
+    
+    @IBAction func notificationDragExitAction(_ sender: Any) {
+        animationContract(button: notifications)
+    }
+    
+    @IBAction func notificationDragEnterAction(_ sender: Any) {
+        animationExpand(button: notifications)
     }
 
 }
