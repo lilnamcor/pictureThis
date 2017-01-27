f6a20be9cfa619dcc968be765d5f6e5cc39a4dd1
Itai Reuveni <lilnamcor@berkeley.edu>
2017-01-26 21:56:37 -0800

    updated searching

--- app/PictureThis/FriendsListController.swift
+++ app/PictureThis/FriendsListController.swift
@@ -8,10 +8,12 @@
 
 import UIKit
 
-class FriendsListController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+class FriendsListController: UIViewController, UITableViewDataSource, UITableViewDelegate, UISearchBarDelegate {
+
 
     @IBOutlet weak var friendsList: UITableView!
-    
+    @IBOutlet weak var searchBar: UISearchBar!
+    @IBOutlet weak var send: UIButton!
     var globalImage = CIImage()
     var blurValue = 0.0
     var zoomValue = 0.0
@@ -19,14 +21,102 @@ class FriendsListController: UIViewController, UITableViewDataSource, UITableVie
     var answer = NSString()
     var xOffset = CGFloat()
     var yOffset = CGFloat()
+    var allFriends = [String]()
+    var displayedFriends = [String]()
+    var selectedFriends = [String]()
+    
+    func searchBarCancelButtonClicked(_ searchBar: UISearchBar) {
+        searchBar.resignFirstResponder()
+        searchBar.text = ""
+        displayedFriends = allFriends
+        friendsList.reloadData()
+        var index = IndexPath()
+        var text = ""
+        for i in 0..<displayedFriends.count {
+            index = IndexPath(row: i, section: 0)
+            if let selectedRow = friendsList.cellForRow(at: index) {
+                text = (selectedRow.textLabel?.text)!
+                if selectedFriends.contains(text) {
+                    friendsList.selectRow(at: index, animated: false, scrollPosition: UITableViewScrollPosition.middle)
+                }
+            }
+        }
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        friendsList.delegate = self
+        searchBar.delegate = self
+        
         friendsList.dataSource = self
         friendsList.allowsMultipleSelection = true
-
+        
+        allFriends = ["Abhi", "Akhil", "Amna", "Byran", "Danielle", "Davit", "Erica", "Jas", "Logan", "Navin", "Ross", "Tim", "Melissa Hsu", "Megan", "Hannah"]
+        displayedFriends = allFriends
+        
         // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func sendAction(_ sender: Any) {
+        print(selectedFriends)
+    }
+    
+    func searchBarTextDidBeginEditing(_ searchBar: UISearchBar) {
+        searchBar.showsCancelButton = false
+    }
+    
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        searchBar.resignFirstResponder()
+    }
+    
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        displayedFriends = filterFriends(friends: allFriends, start: searchText)
+        friendsList.reloadData()
+        var index = IndexPath()
+        var text = ""
+        for i in 0..<displayedFriends.count {
+            index = IndexPath(row: i, section: 0)
+            if let selectedRow = friendsList.cellForRow(at: index) {
+                text = (selectedRow.textLabel?.text)!
+                if selectedFriends.contains(text) {
+                    friendsList.selectRow(at: index, animated: false, scrollPosition: UITableViewScrollPosition.middle)
+                }
+            }
+        }
+    }
+    
+    func filterFriends(friends: [String], start: String) -> [String] {
+        let len = start.endIndex
+        var toReturn = [String]()
+        for friend in friends {
+            if (len < friend.endIndex) {
+                let temp = friend.substring(to: len)
+                if (temp == start as String) {
+                    toReturn.append(friend)
+                }
+            }
+        }
+        return toReturn
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        if let selectedRow = tableView.cellForRow(at: indexPath) {
+            selectedFriends.append((selectedRow.textLabel?.text)!)
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, didDeselectRowAt indexPath: IndexPath) {
+        var selection = ""
+        if let selectedRow = tableView.cellForRow(at: indexPath) {
+            selection = (selectedRow.textLabel?.text)!
+        }
+        let index = selectedFriends.index(of: selection)
+        selectedFriends.remove(at: index!)
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return displayedFriends.count
     }
     
     func numberOfSectionsInTableView(tableView: UITableView) -> Int
@@ -36,7 +126,7 @@ class FriendsListController: UIViewController, UITableViewDataSource, UITableVie
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = UITableViewCell(style: UITableViewCellStyle.default, reuseIdentifier: "td")
-        cell.textLabel?.text = items[indexPath.row]
+        cell.textLabel?.text = displayedFriends[indexPath.row]
         return cell
     }
 
